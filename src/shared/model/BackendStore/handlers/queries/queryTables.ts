b4067d678ef9e95214c4405a7e0002f869543e1c
anton62k <anton.kashirov@gmail.com>
2025-03-15 15:08:20 +0300

    revert

--- src/shared/model/BackendStore/handlers/queries/queryTables.ts
+++ src/shared/model/BackendStore/handlers/queries/queryTables.ts
@@ -19,7 +19,6 @@ export class QueryTablesHandler implements QueryTablesHandlerType {
     const connectionSnapshot = transformConnectionVersionId(result.tables)
 
     result.tables.edges.forEach(({ node }) => {
-      console.log('added to cache', node.id, node.versionId)
       this.store.cache.addOrTable(node)
     })
 

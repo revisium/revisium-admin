24316b5f1a4fec1a3089c247f1e7e1c59b0db521
anton62k <anton.kashirov@gmail.com>
2025-10-09 20:47:07 +0300

    feat: priority object value sorting

--- src/entities/Schema/index.ts
+++ src/entities/Schema/index.ts
@@ -1,4 +1,6 @@
 export * from './types/schema.types.ts'
 export * from 'src/entities/Schema/config/schema-refs-mapper.ts'
 
+export * from './lib'
+
 export { ViewerSwitcherMode } from './config/consts.ts'

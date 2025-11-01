257e1fb2c91b69a2000b336c6829d91685e9420c
anton62k <anton.kashirov@gmail.com>
2025-11-01 12:31:03 +0700

    fixed observer

--- src/pages/BranchPage/ui/ReadonlyBanner/ReadonlyBanner.tsx
+++ src/pages/BranchPage/ui/ReadonlyBanner/ReadonlyBanner.tsx
@@ -1,8 +1,9 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
+import { observer } from 'mobx-react-lite'
 import { Link } from 'react-router-dom'
 import { useLinkMaker } from 'src/entities/Navigation/hooks/useLinkMaker.ts'
 
-export const ReadonlyBanner = () => {
+export const ReadonlyBanner = observer(() => {
   const linkMaker = useLinkMaker()
   const draftLink = linkMaker.make({ isDraft: true })
 
@@ -16,12 +17,12 @@ export const ReadonlyBanner = () => {
       paddingY="4px"
       marginTop="auto"
     >
-      <Flex alignItems="center" justifyContent="center" gap="8px">
+      <Flex alignItems="center" justifyContent="center" gap="16px">
         <Text color="newGray.600">You are viewing a read-only revision</Text>
         <Text color="newGray.400" textDecoration="underline">
-          <Link to={draftLink}>go to draft revision</Link>
+          <Link to={draftLink}>Go to draft revision</Link>
         </Text>
       </Flex>
     </Box>
   )
-}
+})

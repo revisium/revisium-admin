b5b942c6a2ebbf3d26a323a9ec28221baf2a52f1
Anton Kashirov <anton.kashirov@gmail.com>
2025-11-01 12:40:07 +0700

    feat: readonly banner (#146)

--- src/pages/BranchPage/ui/BranchPage/BranchPage.tsx
+++ src/pages/BranchPage/ui/BranchPage/BranchPage.tsx
@@ -3,6 +3,8 @@ import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { useDraftHeadWatcher } from 'src/pages/BranchPage/hooks/useDraftHeadWatcher.ts'
+import { ReadonlyBanner } from 'src/pages/BranchPage/ui/ReadonlyBanner/ReadonlyBanner.tsx'
+import { useProjectPageModel } from 'src/shared/model/ProjectPageModel/hooks/useProjectPageModel.ts'
 import { Page } from 'src/shared/ui'
 import { BranchPageTitleWidget } from 'src/widgets/BranchPageTitleWidget'
 // import { RevisionEndpointWidget } from 'src/widgets/RevisionEndpointWidget'
@@ -11,14 +13,20 @@ import { SidebarWidget } from 'src/widgets/SidebarWidget'
 export const BranchPage: React.FC = observer(() => {
   useDraftHeadWatcher()
 
+  const projectPageModel = useProjectPageModel()
+
+  const showReadonlyBanner = !projectPageModel.isDraftRevision
+
   return (
     <Page sidebar={<SidebarWidget />} title={<BranchPageTitleWidget />}>
-      <Flex flex={1} flexDirection="column" gap="1rem">
+      <Flex flex={1} flexDirection="column" gap="1rem" position="relative">
         {/*<RevisionEndpointWidget />*/}
 
         <Flex flexDirection="column" gap="0.5rem" flex={1}>
           <Outlet />
         </Flex>
+
+        {showReadonlyBanner && <ReadonlyBanner />}
       </Flex>
     </Page>
   )

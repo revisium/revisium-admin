6eaea94a4f94a29bff96c4fe75949b445b96feb8
anton62k <anton.kashirov@gmail.com>
2026-01-11 17:34:57 +0300

    feat: project sidebar skeleton

--- src/widgets/ProjectSidebar/ui/ProjectSidebarSkeleton/ProjectSidebarSkeleton.tsx
+++ src/widgets/ProjectSidebar/ui/ProjectSidebarSkeleton/ProjectSidebarSkeleton.tsx
@@ -8,6 +8,19 @@ interface ProjectSidebarSkeletonProps {
   organizationId: string
 }
 
+const SkeletonRowWithIcon: FC<{ width: string }> = ({ width }) => (
+  <Flex alignItems="center" height="36px" paddingX="8px" gap="12px">
+    <Skeleton height="20px" width="20px" borderRadius="sm" />
+    <Skeleton height="14px" width={width} borderRadius="sm" />
+  </Flex>
+)
+
+const SkeletonRowText: FC<{ width: string }> = ({ width }) => (
+  <Flex alignItems="center" height="36px" paddingX="8px">
+    <Skeleton height="14px" width={width} borderRadius="sm" />
+  </Flex>
+)
+
 export const ProjectSidebarSkeleton: FC<ProjectSidebarSkeletonProps> = ({ projectName, organizationId }) => {
   return (
     <VStack alignItems="flex-start" gap={0} width="100%" flex={1}>
@@ -18,25 +31,14 @@ export const ProjectSidebarSkeleton: FC<ProjectSidebarSkeletonProps> = ({ projec
       </Box>
 
       <Flex flexDirection="column" width="100%" gap={1}>
-        <Skeleton height="36px" width="100%" borderRadius="md" />
-
-        <Box mt={2}>
-          <Skeleton height="36px" width="100%" borderRadius="md" />
-          <Box pl={4} mt={1}>
-            <Skeleton height="32px" width="100%" borderRadius="md" mb={1} />
-            <Skeleton height="32px" width="100%" borderRadius="md" mb={1} />
-            <Skeleton height="32px" width="100%" borderRadius="md" />
-          </Box>
-        </Box>
-
-        <Box mt={2}>
-          <Skeleton height="36px" width="100%" borderRadius="md" />
-          <Box pl={4} mt={1}>
-            <Skeleton height="32px" width="100%" borderRadius="md" mb={1} />
-            <Skeleton height="32px" width="100%" borderRadius="md" mb={1} />
-            <Skeleton height="32px" width="100%" borderRadius="md" />
-          </Box>
-        </Box>
+        <SkeletonRowWithIcon width="60px" />
+        <SkeletonRowText width="70px" />
+        <SkeletonRowWithIcon width="80px" />
+        <SkeletonRowWithIcon width="70px" />
+        <SkeletonRowWithIcon width="90px" />
+        <SkeletonRowWithIcon width="90px" />
+        <SkeletonRowText width="85px" />
+        <SkeletonRowText width="85px" />
       </Flex>
 
       <Spacer />

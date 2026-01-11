bd6adbc92e16b0272685c63499810e97890f5f86
Anton Kashirov <anton.kashirov@gmail.com>
2026-01-11 15:15:46 +0300

    chore: remove get revision changes (#258)

--- src/widgets/ProjectSidebar/model/ProjectSidebarViewModel.ts
+++ src/widgets/ProjectSidebar/model/ProjectSidebarViewModel.ts
@@ -1,19 +1,12 @@
 import { makeAutoObservable } from 'mobx'
 import { ProjectContext } from 'src/entities/Project/model/ProjectContext.ts'
 import { container } from 'src/shared/lib'
-import { ObservableRequest } from 'src/shared/lib/ObservableRequest.ts'
 import { PermissionContext } from 'src/shared/model/AbilityService'
-import { client } from 'src/shared/model/ApiService.ts'
 
 export class ProjectSidebarViewModel {
   public isBranchSectionExpanded = true
   public isBranchesSectionExpanded = false
   public isProjectSectionExpanded = false
-  public isOnSettingsPage = false
-
-  private readonly getRevisionChangesRequest = ObservableRequest.of(client.GetRevisionChanges, {
-    skipResetting: true,
-  })
 
   constructor(
     private readonly context: ProjectContext,
@@ -46,25 +39,9 @@ export class ProjectSidebarViewModel {
     return this.permissionContext.projectRoleName
   }
 
-  public init() {
-    void this.loadChangesCount()
-  }
+  public init() {}
 
   public dispose() {}
-
-  private async loadChangesCount(): Promise<void> {
-    if (!this.context.isDraftRevision) {
-      return
-    }
-    try {
-      await this.getRevisionChangesRequest.fetch({
-        revisionId: this.context.revisionId,
-        includeSystem: true,
-      })
-    } catch (e) {
-      console.error('Failed to load changes count', e)
-    }
-  }
 
   public handleBranchSectionClick() {
     this.isBranchSectionExpanded = !this.isBranchSectionExpanded

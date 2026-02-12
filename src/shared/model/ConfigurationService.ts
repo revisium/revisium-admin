44385f86417a6352c0b0a4501af7713ebbd6f8b8
anton62k <anton.kashirov@gmail.com>
2026-02-12 23:32:36 +0300

    feat: no auth

--- src/shared/model/ConfigurationService.ts
+++ src/shared/model/ConfigurationService.ts
@@ -49,6 +49,10 @@ export class ConfigurationService {
     return this.data?.github.clientId
   }
 
+  public get noAuth(): boolean {
+    return this.data?.noAuth ?? false
+  }
+
   public get formulaEnabled(): boolean {
     return this.data?.plugins?.formula ?? false
   }
